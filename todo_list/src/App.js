4efa2ed4c7d10da8df8d2cf8fcca15835edbcc86
facuB <facuB@example.com>
2023-02-08 19:46:06 -0500

    Todo list update

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,29 +1,117 @@
 import React from "react";
 import "./App.css";
+
 const App = () => {
-  const [todos, setTodos] = React.useState([]);
-  const [todo, setTodo] = React.useState("");
-  
-  // Add the handlesubmit code here
-  
-  
-  // Add the deleteToDo code here
+    const [todos, setTodos] = React.useState([]);
+    const [todo, setTodo] = React.useState("");
 
-  
-  // Add the toggleComplete code here
+    const [todoEditing, setTodoEditing] = React.useState(null);
+    const [editingText, setEditingText] = React.useState("");
 
-  
-  // Add the submitEdits code here
+    React.useEffect(() => {
+        const json = localStorage.getItem("todos");
+        const loadedTodos = JSON.parse(json);
+        if (loadedTodos) {
+            setTodos(loadedTodos);
+        }
+    }, []);
 
-  
-return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" />
-<button type ="submit">Add Todo</button>
-</form>
-</div>
-);
+    React.useEffect(() => {
+        const json = JSON.stringify(todos);
+        localStorage.setItem("todos", json);
+    }, [todos]);
+
+    // Add the handlesubmit code here
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        const newTodo = {
+            id: new Date().getTime(),
+            text: todo.trim(),
+            completed: false,
+        };
+        if (newTodo.text.length > 0) {
+            setTodos([...todos].concat(newTodo));
+            setTodo("");
+
+        } else {
+
+            alert("Enter Valid Task");
+            setTodo("");
+        }
+    }
+
+    // Add the deleteToDo code here
+    function deleteTodo(toDelete) {
+        const newTodos = todos.filter(element => element !== toDelete);
+        setTodos(newTodos);
+    }
+
+    // Add the toggleComplete code here
+    function toggleComplete(id) {
+        let updatedTodos = [...todos].map((todo) => {
+            if (todo.id === id) {
+                todo.completed = !todo.completed;
+            }
+            return todo;
+        });
+        setTodos(updatedTodos);
+    }
+
+    // Add the submitEdits code here
+    function submitEdits(toEditId) {
+        const editedTodos = todos.map(element => {
+            if (element.id === toEditId) {
+                element.text = editingText
+            }
+            return element;
+        })
+        setTodos(editedTodos);
+        setTodoEditing(null);
+        setEditingText("");
+    }
+
+    return (
+        <div id="todo-list">
+            <h1>Todo List</h1>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    onChange={(e) => setTodo(e.target.value)}
+                    value={todo}
+                />
+                <button type="submit">Add Todo</button>
+            </form>
+            {todos.map((todo) => (
+                <div key={todo.id} className="todo">
+                    <div className="todo-text">
+                        <input
+                            type="checkbox"
+                            id="completed"
+                            checked={todo.completed}
+                            onChange={() => toggleComplete(todo.id)}
+                        />
+                        {todo.id === todoEditing ? (
+                            <input
+                                type="text"
+                                onChange={(e) => setEditingText(e.target.value)}
+                            />
+                        ) : (
+                                <div>{todo.text}</div>
+                            )}
+                    </div>
+                    <div className="todo-actions">
+                        {todo.id === todoEditing ? (
+                            <button onClick={() => submitEdits(todo.id)}>Submit Edits</button>
+                        ) : (
+                                <button onClick={() => setTodoEditing(todo.id)}>Edit</button>
+                            )}
+
+                        <button onClick={() => deleteTodo(todo)}>Delete</button>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 };
 export default App;
